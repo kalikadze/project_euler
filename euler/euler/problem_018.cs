f0fe0b73ccc40b2a7d7cbd4c913f0aeccd369490
kalikadze <jozef.kalianko@gmail.com>
2018-01-15 23:28:22 +0100

    problems 18, 67 solved
    max path triangles

--- euler/euler/problem_018.cs
+++ euler/euler/problem_018.cs
@@ -8,35 +8,12 @@ namespace euler
 {
     class problem_018
     {
-        /// <summary>
-        /// divide & weigth recursive function
-        /// </summary>
-        /// <param name="x">x coordinate</param>
-        /// <param name="y">y coordinate</param>
-        /// <param name="data">triangle structure</param>
-        /// <returns>-1 - error, 0 - end, 1 - left, 2 - right</returns>
-        int weight(int x, int y, List<List<int>> data)
-        {
-            int sum = 0;
-            int yy = 0;
-            for (int n = y; n < data.Count; n++)
-            {
-                yy++;
-                for (int m = x; m < x + yy; m++)
-                {
-                    sum += data[n][m];
-                }
-            }
-            return sum;
-        }
-
         public problem_018()
         {
             string f = @"..\..\problem_018.in";
             List<int> linenum = new List<int>();
             List<List<int>> nums = new List<List<int>>();
-            int Sum = 0;
-            int x = 0, y = 0;
+            List<List<int>> sumnums;
 
             using (StreamReader r = new StreamReader(f))
             {
@@ -47,33 +24,22 @@ namespace euler
                     nums.Add(linenum);
                 }
             }
+            sumnums = new List<List<int>>(nums);
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            Sum = nums[x][y];
-
-            for (int i = 0; i < nums.Count - 1; i++)
-            {
-                int strom = weight(0, 0, nums);
-                int left = weight(x, y + 1, nums);
-                int right = weight(x + 1, y + 1, nums);
-
-                if ( left > right)
+            for (int n = nums.Count - 1; n > 0; n--)
+                for (int m = nums[n].Count -1; m > 0; m--)
                 {
-                    y++;
-                    Sum += nums[y][x];
+                    if (nums[n][m] > nums[n][m - 1])
+                        nums[n - 1][m - 1] += nums[n][m];
+                    else
+                        nums[n - 1][m - 1] += nums[n][m - 1];
                 }
-                else
-                {
-                    x++;
-                    y++;
-                    Sum += nums[y][x];
-                }
-            }
 
             Console.WriteLine("Problem 018");
-            Console.WriteLine(Sum);
+            Console.WriteLine(nums[0][0]);
             sw.Stop();
             long ts = sw.ElapsedMilliseconds;
             Console.WriteLine("Time elapsed: {0} ms", ts);
