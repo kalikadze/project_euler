82a5e953e33c7c4f9717c32a0adda988cf7f4b95
kalikadze <jozef.kalianko@gmail.com>
2018-03-26 16:05:06 +0200

    50 done
    need to improve
    non effective: exec time = 60340ms

--- euler/euler/problem_050.cs
+++ euler/euler/problem_050.cs
@@ -10,46 +10,53 @@ namespace euler
     {
         public problem_050()
         {
-            int sum = 0;
-            int max = 0;
-            int maxlen = 0;
-            List<int> primes = new List<int>();
-            int border = 1000000;
+            long sum = 0;
+            long max = 0;
+            long maxlen = 0;
+            List<long> primes = new List<long>();
+            List<long> tempy = new List<long>();
+            long border = 1000000;
+            long allprimsum = 0;
             
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            for (int i = 2; i < border; i++)
+            for (long i = 2; i < border; i++)
             {
                 if (Utils.isPrime(i))
                     primes.Add(i);
             }
-            
-            for (int i = 0; i < primes.Count; i++)
-            {
-                for (int k = 0; k < i; k++)
-                {
-                    sum = 0;
-                    for (int j = k; j < i; j++)
-                    {
-                        sum += primes[j];
-                        if (sum == primes[i])
-                        {
-                            if (j - k + 1 > maxlen)
-                            {
-                                maxlen = j - k + 1;
-                                max = sum;
-                            }
 
-                            if (sum > primes[i])
-                                break;
+            //allprimsum = primes.Sum(ind => (int)ind);
+            //sum = allprimsum;
+
+
+            for (long i = primes.Count - 1; i > -1; i--)
+            {
+                sum = 0;
+                for (long k = i; k > -1; k--)
+                    sum += primes[(int)k];
+
+                if (sum > border)
+                    continue;
+                
+                for (int j = 0; j < i; j++)
+                {
+                    if (primes.Contains(sum))
+                    {
+                        if (i - j > maxlen)
+                        {
+                            max = sum;
+                            maxlen = i - j;
                         }
                     }
+
+                    sum -= primes[j];
                 }
             }
             
             Console.WriteLine("Problem 050");
-            Console.WriteLine();
+            Console.WriteLine(max);
             sw.Stop();
             long ts = sw.ElapsedMilliseconds;
             Console.WriteLine("Time elapsed: {0} ms", ts);
