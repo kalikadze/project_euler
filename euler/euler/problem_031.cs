7bcd138c958143fcc58851d1d1aedf51407e05f3
kalikadze <jozef.kalianko@gmail.com>
2018-04-03 23:21:10 +0200

    31 done

--- euler/euler/problem_031.cs
+++ euler/euler/problem_031.cs
@@ -8,14 +8,35 @@ namespace euler
 {
     class problem_031
     {
+        int[] coins = { 200, 100, 50, 20, 10, 5, 2, 1 };
+
+        int findways(int money, int maxcoin)
+        {
+            int sum = 0;
+            if (maxcoin == 7) return 1;
+            for (int i = maxcoin; i < 8; i++)
+            {
+                if (money - coins[i] == 0)
+                    sum += 1;
+                if (money - coins[i] > 0)
+                    sum += findways(money - coins[i], i);
+            }
+            return sum;
+        }
+
+
         public problem_031()
         {
-            // dumb solution - BF
             int cnt = 0;
-            double sum = 0;
+            int sum = 0;
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
+
+            cnt = findways(200, 0);
+
+            /*
+            // dumb solution - BF
 
             for (int a = 0; a < 2; a++)                                 // 2
                 for (int b = 0; b < 3; b++)                             // 1
@@ -26,15 +47,16 @@ namespace euler
                                     for (int g = 0; g < 101; g++)       // 0.02
                                         for (int h = 0; h < 201; h++)   // 0.01
                                         {
-                                            sum = a * 2 + b * 1 + c * 0.5 + d * 0.2 + e * 0.1 + f * 0.05 + g * 0.02 + h * 0.01;
+                                            sum = a * 200 + b * 100 + c * 50 + d * 20 + e * 10 + f * 5 + g * 2 + h * 1;
 
-                                            if (sum > 2)
+                                            if (sum > 200)
                                                 continue;
 
-                                            if (sum == 2)
+                                            if (sum == 200)
                                                 cnt++;
 
                                         }
+            */
 
             Console.WriteLine("Problem 039");
             Console.WriteLine(cnt);
