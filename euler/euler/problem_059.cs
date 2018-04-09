dbd63a2e63ded3a8e813df39f66faa1567103f41
kalikadze <jozef.kalianko@gmail.com>
2018-04-09 16:04:36 +0200

    59 ongoing :)

---
+++ euler/euler/problem_059.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+
+namespace euler
+{
+    class problem_059
+    {
+        public problem_059()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+
+            Console.WriteLine("Problem 059");
+            Console.WriteLine();
+            sw.Stop();
+            long ts = sw.ElapsedMilliseconds;
+            Console.WriteLine("Time elapsed: {0} ms", ts);
+            Console.WriteLine(Utils.sep);
+        }
+    }
+}
